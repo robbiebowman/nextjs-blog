e0fc40423954b13837e9afeb90ac19a416c24307
Robbie <robbiebowman@gmail.com>
2022-02-04 00:12:28 -0500

    Serve splash screen with socials

---
+++ components/layout.js
@@ -0,0 +1,89 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import React, { useState } from 'react';
+
+const name = 'Robbie Bowman'
+export const siteTitle = 'Robbie Bowman dot com'
+
+const emailPortionA = 'robbiebo'
+const emailPortionB = 'wman'
+const emailPortionC = '@gmail.com'
+
+export default function Layout({ children, home }) {
+    const [showEmail, setShowEmail] = useState(false)
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/images/favicon.ico" />
+                <meta
+                    property="og:image"
+                    content={`https://og-image.vercel.app/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <header className={styles.header}>
+                <div className={styles.avatarContainer}>
+                    <Link href="/">
+                        <a>
+                            <Image
+                                priority
+                                src="/images/13639947.jpg"
+                                className={utilStyles.borderCircle}
+                                height={108}
+                                width={108}
+                                alt={name}
+                            />
+                        </a>
+                    </Link>
+                    <h2 className={utilStyles.headingLg}>
+                        <Link href="/">
+                            <a className={utilStyles.colorInherit}>{name}</a>
+                        </Link>
+                    </h2>
+                </div>
+                <div className={styles.headerLinks}>
+                    <Link href="https://github.com/robbiebowman/">
+                        <a><div><div className={styles.socialIcon}><Image src="/images/github.webp" height={14} width={14} /></div>Github</div></a>
+                    </Link>
+                    {
+                        showEmail
+                            ? <div>
+                                <div className={styles.socialIcon}>
+                                    <Image src="/images/gmail.webp" height={14} width={14} />
+                                </div>
+                                {emailPortionA}{emailPortionB}{emailPortionC}
+                            </div>
+                            : <a onClick={() => setShowEmail(true)}>
+                                <div>
+                                    <div className={styles.socialIcon}>
+                                        <Image src="/images/gmail.webp" height={14} width={14} />
+                                    </div>
+                                    Email
+                                </div>
+                            </a>
+                    }
+                    <Link href="https://www.linkedin.com/in/robbie-bowman/">
+                        <a><div><div className={styles.socialIcon}><Image src="/images/linkedin.webp" height={14} width={14} /></div>LinkedIn</div></a>
+                    </Link>
+                    <Link href="https://www.instagram.com/robbiebowman/">
+                        <a><div><div className={styles.socialIcon}><Image src="/images/instagram.webp" height={14} width={14} /></div>Instagram</div></a>
+                    </Link>
+                </div>
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a>← Back to home</a>
+                    </Link>
+                </div>
+            )}
+        </div>
+    )
+}
