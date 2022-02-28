69b6c62f4d098e6d699aa2d1f6177eaec792fad7
Robbie <robbiebowman@gmail.com>
2022-02-28 00:42:53 -0500

    Fix azure function body

--- api/chess-position/index.js
+++ api/chess-position/index.js
@@ -2,18 +2,9 @@ module.exports = async function (context, req) {
 
     const response = await fetch(process.env.PERSONAL_API_URL + "/chess-evals?difficulty=Medium").then(res => res.json())
 
-    console.log(response)
-    // req = HTTP incoming message, res = HTTP server response
-    res.status(200).json(response)
-
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        fen: response.fen,
+        evaluation: response.evaluation
     };
 }
