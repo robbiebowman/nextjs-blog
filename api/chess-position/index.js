9ff9370382b32bf1952ed36769502d43da268aa2
Robbie <robbiebowman@gmail.com>
2022-02-28 01:02:37 -0500

    Just send the response

--- api/chess-position/index.js
+++ api/chess-position/index.js
@@ -2,9 +2,5 @@ module.exports = async function (context, req) {
 
     const response = await fetch("https://rjb-personal-api.azurewebsites.net/chess-evals?difficulty=Medium").then(res => res.json())
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        fen: response.fen,
-        evaluation: response.evaluation
-    };
+    context.res = {response};
 }
