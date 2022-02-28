61be1e602ba045ca16e245f3c131de84535604af
Robbie <robbiebowman@gmail.com>
2022-02-28 00:58:02 -0500

    Don't use env vars in azure functions

--- api/chess-position/index.js
+++ api/chess-position/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
 
-    const response = await fetch(process.env.PERSONAL_API_URL + "/chess-evals?difficulty=Medium").then(res => res.json())
+    const response = await fetch("https://rjb-personal-api.azurewebsites.net/chess-evals?difficulty=Medium").then(res => res.json())
 
     context.res = {
         // status: 200, /* Defaults to 200 */
