b3cfe4445a1df246be400437946177cb43325623
Robbie <robbiebowman@gmail.com>
2022-02-28 21:36:33 -0500

    Add test apis to debug error

--- pages/api/api-test.js
+++ pages/api/api-test.js
@@ -2,8 +2,10 @@ import useSWR from 'swr';
 
 export default async function handler(req, res) {
 
+  const response = await fetch("https://nextjs.org/docs/api-routes/introduction")
+
   // req = HTTP incoming message, res = HTTP server response
   res.status(200).json({
-    data:"Hello Bobby"
+    response: response
   })
 }
