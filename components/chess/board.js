cc69943685bdf5b2bdc67bb52044aeb739691780
Robbie <robbiebowman@gmail.com>
2022-03-04 22:31:00 -0500

    Extract board into own component

---
+++ components/chess/board.js
@@ -0,0 +1,47 @@
+import styles from './chess.module.css'
+import { loadNewRandomPuzzle, isCorrect } from '/lib/chess'
+import { Chessboard } from "react-chessboard"
+import EvalBar from "./eval-bar/eval-bar"
+import { useState, useEffect, createRef } from 'react'
+
+export default function Board({ data, difficulty, onCorrect, onWrong, showResults }) {
+
+    const usedForGettingRemSize = createRef()
+    const [boardWidth, setBoardWidth] = useState(400)
+
+    const selectAnswer = (answer) => {
+        if (isCorrect(data.evaluation, answer)) {
+            onCorrect(answer)
+        } else {
+            onWrong(answer)
+        }
+    }
+
+    useEffect(() => {
+        const width = parseFloat(getComputedStyle(usedForGettingRemSize.current).width);
+        setBoardWidth(Math.min(width, 500))
+    }, []);
+
+    return (<>
+        <div className={styles.boardAndEval} ref={usedForGettingRemSize}>
+            <div className={showResults ? styles.visibleEval : styles.invisibleEval}><EvalBar evaluation={data.evaluation} /></div>
+            <Chessboard boardWidth={boardWidth} arePiecesDraggable={false} position={data.fen} />
+            <div className={styles.thingToMakeBoardSymmetrical} />
+        </div>
+        <div className={styles.guessBox}>
+            <button type="button"
+                className="btn btn-light"
+                disabled={showResults}
+                onClick={() => selectAnswer("+")}>White</button>
+            <button type="button"
+                className={"btn btn-secondary " + (difficulty == 'Hard' ? styles.visibleButton : styles.hiddenButton)}
+                disabled={showResults || difficulty != 'Hard'}
+                onClick={() => selectAnswer("=")}>Even</button>
+            <button type="button"
+                className="btn btn-dark"
+                disabled={showResults}
+                onClick={() => selectAnswer("-")}>Black</button>
+        </div>
+    </>)
+
+}
--- components/chess/chess-position-guesser.js
+++ components/chess/chess-position-guesser.js
@@ -1,42 +1,29 @@
-import useSWR from 'swr';
-import { Chessboard } from "react-chessboard";
-import EvalBar from "./eval-bar/eval-bar";
+import useSWR from 'swr'
 import DifficultySelector from "./difficulty-selector/difficulty-selector"
 import styles from './chess.module.css'
-import { useState, useEffect, createRef } from 'react';
-import Score from './score/score';
+import { useState, useEffect, createRef } from 'react'
+import { loadNewRandomPuzzle, isCorrect } from '/lib/chess'
+import Score from './score/score'
+import Board from './board'
 
-export default function ChessPositionGuesser({mode}) {
+export default function ChessPositionGuesser({ mode }) {
     // mode: ["Practice", "Daily", "Storm"]
 
-    const usedForGettingRemSize = createRef()
     const [data, setData] = useState({ fen: "", evaluation: 0 })
     const [isLoading, setLoading] = useState(false)
     const [difficulty, setDifficulty] = useState("Medium")
     const [newPuzzleRequested, setNewPuzzleRequested] = useState(true)
-    const [guessed, setGuessed] = useState(false)
+    const [showResults, setShowResults] = useState(false)
     const [answer, setAnswer] = useState(null)
     const [whomToMove, setWhomToMove] = useState("...")
-    const [boardWidth, setBoardWidth] = useState(400)
     const [streak, setStreak] = useState(0)
-
-    const isCorrect = (evaluation, answer) => {
-        const wasMate = evaluation[0] == '#'
-        var wasCorrect
-        if (wasMate) {
-            wasCorrect = answer == evaluation[1]
-        } else {
-            wasCorrect = (answer == "+" && evaluation > 0) || (answer == "-" && evaluation < 0) || (answer == "=" && evaluation == 0)
-        }
-        return wasCorrect
-    }
 
     useEffect(() => {
         if (newPuzzleRequested && !isLoading) {
             setLoading(true)
-            fetch("/api/chess-position?difficulty=" + difficulty).then(res => res.json()).then((data) => {
+            loadNewRandomPuzzle(difficulty).then((data) => {
                 const move = data.fen.split(' ')[1] == 'w' ? "⬜ White" : "⬛ Black"
-                setGuessed(false)
+                setShowResults(false)
                 setData(data)
                 setLoading(false)
                 setNewPuzzleRequested(false)
@@ -45,38 +32,30 @@ export default function ChessPositionGuesser({mode}) {
         }
     }, [newPuzzleRequested])
 
-    const selectAnswer = (answer) => {
-        setGuessed(true)
+    const selectAnswer = (answer, correct) => {
+        setShowResults(true)
         setAnswer(answer)
-        if (isCorrect(data.evaluation, answer)) {
+        if (correct) {
             setStreak(streak + 1)
         } else {
             setStreak(0)
         }
     }
 
-    useEffect(() => {
-        const width = parseFloat(getComputedStyle(usedForGettingRemSize.current).width);
-        setBoardWidth(Math.min(width, 500))
-    }, []);
-
     return (
-        <div className={styles.boardBox} ref={usedForGettingRemSize}>
+        <div className={styles.boardBox}>
             <div className={styles.difficultyAndWhomToMove}>
                 <div className={styles.whomToMove}><span>{`${whomToMove} to move`}</span></div>
                 <DifficultySelector setDifficulty={(level) => { setDifficulty(level) }} />
             </div>
-            <div className={styles.boardAndEval}>
-                <div className={guessed ? styles.visibleEval : styles.invisibleEval}><EvalBar evaluation={data.evaluation} /></div>
-                <Chessboard boardWidth={boardWidth} arePiecesDraggable={false} position={data.fen} />
-                <div className={styles.thingToMakeBoardSymmetrical} />
-            </div>
-            <div className={styles.guessBox}>
-                <button type="button" className="btn btn-light" disabled={guessed} onClick={() => selectAnswer("+")}>White</button>
-                <button type="button" className={"btn btn-secondary " + (difficulty == 'Hard' ? styles.visibleButton : styles.hiddenButton)} disabled={guessed} onClick={() => selectAnswer("=")}>Even</button>
-                <button type="button" className="btn btn-dark" disabled={guessed} onClick={() => selectAnswer("-")}>Black</button>
-            </div>
-            <div className={guessed ? styles.visibleEval : styles.invisibleEval}>
+            <Board
+                data={data}
+                difficulty={difficulty}
+                showResults={showResults}
+                onCorrect={(answer) => { selectAnswer(answer, true) }}
+                onWrong={(answer) => { selectAnswer(answer, false) }}
+            />
+            <div className={showResults ? styles.visibleEval : styles.invisibleEval}>
                 <Score answer={answer}
                     fen={data.fen}
                     evaluation={data.evaluation}
