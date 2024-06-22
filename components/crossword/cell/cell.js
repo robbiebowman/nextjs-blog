8e4a7ded78566a3f739cbd705c816cfbd6e6e84e
Robbie Bowman <robbiebowman@gmail.com>
2024-06-22 17:55:49 -0400

    Reorder stuff

--- components/crossword/cell/cell.js
+++ components/crossword/cell/cell.js
@@ -30,6 +30,8 @@ export default function Cell({ letter, onClick, isHighlightedRow, isActiveCell, 
 
   return (
     <div className={boxStyle} style={{ aspectRatio: 1 }} onClick={handleClick}>
+      <div className={styles.number}>{number ?? ""}</div>
+      <div className={styles.innerLetter}>{letter.toLowerCase()}</div>
       <input
         className={styles.mobileInput}
         ref={inputRef}
@@ -38,8 +40,6 @@ export default function Cell({ letter, onClick, isHighlightedRow, isActiveCell, 
         readOnly={true}
         inputMode="text"
       />
-      <div className={styles.number}>{number ?? ""}</div>
-      <div className={styles.innerLetter}>{letter.toLowerCase()}</div>
     </div>
   )
 }
