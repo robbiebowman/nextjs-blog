14d8f8a5ececd69a93b8207b220aef050b9dc7f2
Robbie Bowman <robbiebowman@gmail.com>
2024-06-22 18:53:27 -0400

    Put each input in its own form

--- components/crossword/cell/cell.js
+++ components/crossword/cell/cell.js
@@ -31,21 +31,21 @@ export default function Cell({ letter, onClick, isHighlightedRow, isActiveCell, 
   return (
     <div className={boxStyle} style={{ aspectRatio: 1 }} onClick={handleClick}>
       {number && <div className={styles.number}>{number}</div>}
-      <input
-        className={styles.mobileInput}
-        ref={inputRef}
-        type="text"
-        inputMode="text"
-        autoComplete="off"
-        autoCorrect="off"
-        autoCapitalize="off"
-        spellCheck="false"
-        data-quicktypes="off"
-        maxLength="1"
-        value={letter || ''}
-        onChange={handleInput}
-        onInput={handleInput}
-      />
+      <form>
+        <input
+          className={styles.mobileInput}
+          ref={inputRef}
+          type="text"
+          inputMode="text"
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          data-quicktypes="off"
+          onChange={handleInput}
+          onInput={handleInput}
+        />
+      </form>
       <div className={styles.innerLetter}>{letter.toLowerCase()}</div>
     </div>
   )
